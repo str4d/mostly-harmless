c922c289fa6cef4e9ae28efb2a88fb59fe773a76
Jack Grigg <thestr4d@gmail.com>
2023-07-26 00:21:41 +0000

    atp.fyi: Build unofficial Bluesky roadmap from GitHub issues

--- str4d.fly.dev/src/atp_fyi.rs
+++ str4d.fly.dev/src/atp_fyi.rs
@@ -1,8 +1,13 @@
 use askama::Template;
 use axum::{routing::get, Router};
+use cached::proc_macro::cached;
+
+mod github;
 
 pub(crate) fn build() -> Router {
-    Router::new().route("/", get(index))
+    Router::new()
+        .route("/", get(index))
+        .route("/roadmap", get(roadmap))
 }
 
 #[derive(Clone, Template)]
@@ -11,4 +16,22 @@ struct Index {}
 
 async fn index() -> Index {
     Index {}
+}
+
+#[derive(Clone, Template)]
+#[template(path = "atp.fyi/roadmap.html")]
+struct Roadmap {
+    roadmap: Option<github::Roadmap>,
+}
+
+#[cached(time = 60)]
+async fn roadmap() -> Roadmap {
+    let roadmap = match self::github::get_roadmap().await {
+        Ok(roadmap) => Some(roadmap),
+        Err(e) => {
+            tracing::error!("Failed to get roadmap: {:?}", e);
+            None
+        }
+    };
+    Roadmap { roadmap }
 }
---
+++ str4d.fly.dev/src/atp_fyi/github.rs
@@ -0,0 +1,171 @@
+use std::env;
+
+use graphql_client::{GraphQLQuery, Response};
+
+use self::social_app_query::SocialAppQueryRepositoryIssuesEdgesNodeLabelsEdges;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "res/graphql/github-schema.json",
+    query_path = "res/graphql/social-app-query.graphql"
+)]
+pub struct SocialAppQuery;
+
+pub(super) async fn get_roadmap() -> Result<Roadmap, Error> {
+    let variables = social_app_query::Variables;
+    let request_body = SocialAppQuery::build_query(variables);
+
+    let client = reqwest::ClientBuilder::new()
+        .user_agent("atp.fyi")
+        .build()?;
+    let res = client
+        .post("https://api.github.com/graphql")
+        .bearer_auth(env::var("GITHUB_API_KEY")?)
+        .json(&request_body)
+        .send()
+        .await?
+        .error_for_status()?;
+    let response_body: Response<social_app_query::ResponseData> = res.json().await?;
+
+    let data = response_body.data.ok_or_else(|| {
+        if let Some(errors) = response_body.errors {
+            Error::GraphQL(errors)
+        } else {
+            Error::UnknownGraphQLError
+        }
+    })?;
+    let repo = data.repository.expect("repo exists");
+
+    let mut roadmap = Roadmap::default();
+
+    for (label, author, issue) in repo
+        .issues
+        .edges
+        .into_iter()
+        .flat_map(|issues| issues.into_iter())
+        .map(|e| e.and_then(|edge| edge.node))
+        .flatten()
+        .filter_map(|issue| {
+            issue
+                .labels
+                .and_then(|labels| labels.edges)
+                .and_then(|labels| labels.into_iter().find_map(Label::parse))
+                .map(|label| {
+                    (
+                        label,
+                        Author::parse(issue.author),
+                        Issue::new(issue.number, issue.title),
+                    )
+                })
+        })
+    {
+        match label {
+            Label::Discussing => roadmap.discussing.push(author, issue),
+            Label::OnTheRoadmap => roadmap.on_roadmap.push(author, issue),
+            Label::Planned => roadmap.planned.push(author, issue),
+            Label::PuttingThisOff => roadmap.putting_off.push(author, issue),
+        }
+    }
+
+    Ok(roadmap)
+}
+
+enum Label {
+    Discussing,
+    OnTheRoadmap,
+    Planned,
+    PuttingThisOff,
+}
+
+impl Label {
+    fn parse(val: Option<SocialAppQueryRepositoryIssuesEdgesNodeLabelsEdges>) -> Option<Self> {
+        let label = val?.node?.name;
+        match label.as_str() {
+            "x:discussing" => Some(Label::Discussing),
+            "x:on-the-roadmap" => Some(Label::OnTheRoadmap),
+            "x:planned" => Some(Label::Planned),
+            "x:putting-this-off" => Some(Label::PuttingThisOff),
+            _ => None,
+        }
+    }
+}
+
+enum Author {
+    Devs,
+    Community,
+}
+
+impl Author {
+    fn parse(val: Option<social_app_query::SocialAppQueryRepositoryIssuesEdgesNodeAuthor>) -> Self {
+        val.map_or(Author::Community, |author| match author.login.as_str() {
+            "pfrazee" | "ansh" | "renahlee" | "bnewbold" | "Jacob2161" => Author::Devs,
+            _ => Author::Community,
+        })
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub(super) struct Roadmap {
+    pub(super) discussing: Issues,
+    pub(super) planned: Issues,
+    pub(super) on_roadmap: Issues,
+    pub(super) putting_off: Issues,
+}
+
+#[derive(Clone, Debug, Default)]
+pub(super) struct Issues {
+    pub(super) devs: Vec<Issue>,
+    pub(super) community: Vec<Issue>,
+}
+
+impl Issues {
+    fn push(&mut self, author: Author, issue: Issue) {
+        match author {
+            Author::Devs => self.devs.push(issue),
+            Author::Community => self.community.push(issue),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub(super) struct Issue {
+    number: i64,
+    title: String,
+}
+
+impl Issue {
+    fn new(number: i64, title: String) -> Self {
+        Issue { number, title }
+    }
+
+    pub(super) fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub(super) fn uri(&self) -> String {
+        format!(
+            "https://github.com/bluesky-social/social-app/issue/{}",
+            self.number
+        )
+    }
+}
+
+#[derive(Debug)]
+pub(super) enum Error {
+    Reqwest(reqwest::Error),
+    GraphQL(Vec<graphql_client::Error>),
+    UnknownGraphQLError,
+    Var(env::VarError),
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::Reqwest(err)
+    }
+}
+
+impl From<env::VarError> for Error {
+    fn from(err: env::VarError) -> Self {
+        Error::Var(err)
+    }
+}
