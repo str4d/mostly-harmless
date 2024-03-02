3942c1f20445767820914418f8b2d54f8e08ca46
Jack Grigg <thestr4d@gmail.com>
2024-03-02 18:12:33 +0000

    siso.dev: Migrate to atrium

--- str4d.fly.dev/src/siso_dev.rs
+++ str4d.fly.dev/src/siso_dev.rs
@@ -1,8 +1,13 @@
 use askama::Template;
+use atrium_api::{
+    app::{self, bsky::feed::post::RecordEmbedEnum::AppBskyEmbedImagesMain},
+    client::AtpServiceClient,
+    com::atproto::repo::list_records,
+    types::{BlobRef, TypedBlobRef},
+};
+use atrium_xrpc_client::reqwest::ReqwestClientBuilder;
 use axum::{routing::get, Router};
 use cached::proc_macro::cached;
-
-mod atproto;
 
 pub(crate) fn build() -> Router {
     Router::new().route("/", get(index))
@@ -11,12 +16,12 @@ pub(crate) fn build() -> Router {
 #[derive(Clone, Template)]
 #[template(path = "siso.dev/index.html")]
 struct Index {
-    feed: Vec<(String, atproto::Post)>,
+    feed: Vec<(String, Post)>,
 }
 
 #[cached(time = 60)]
 async fn index() -> Index {
-    let feed = match atproto::Client::new().get_feed().await {
+    let feed = match get_feed().await {
         Ok(feed) => feed,
         Err(e) => {
             tracing::error!("Failed to get feed: {:?}", e);
@@ -24,4 +29,87 @@ async fn index() -> Index {
         }
     };
     Index { feed }
+}
+
+async fn get_feed() -> anyhow::Result<Vec<(String, Post)>> {
+    let client = AtpServiceClient::new(
+        ReqwestClientBuilder::new("https://bsky.social")
+            .client(reqwest::ClientBuilder::new().use_rustls_tls().build()?)
+            .build(),
+    );
+
+    let feed = client
+        .service
+        .com
+        .atproto
+        .repo
+        .list_records(list_records::Parameters {
+            collection: "app.bsky.feed.post".parse().expect("valid"),
+            cursor: None,
+            limit: Some(10.try_into().expect("valid")),
+            repo: "siso.dev".parse().expect("valid"),
+            reverse: None,
+            rkey_end: None,
+            rkey_start: None,
+        })
+        .await?;
+
+    Ok(feed
+        .records
+        .into_iter()
+        .filter_map(|r| match r.value {
+            atrium_api::records::Record::AppBskyFeedPost(post) => {
+                Some((r.cid.as_ref().to_string(), Post(*post)))
+            }
+            _ => None,
+        })
+        .collect())
+}
+
+#[derive(Clone)]
+pub(super) struct Post(app::bsky::feed::post::Record);
+
+impl Post {
+    pub(super) fn created_at(&self) -> &chrono::DateTime<chrono::FixedOffset> {
+        self.0.created_at.as_ref()
+    }
+
+    pub(super) fn formatted_text(&self) -> String {
+        format!(
+            "<p>{}</p>",
+            self.0.text.replace("\n\n", "</p><p>").replace("\n", "<br>")
+        )
+    }
+
+    pub(super) fn images(&self) -> impl Iterator<Item = (String, &String)> + '_ {
+        self.0
+            .embed
+            .iter()
+            .filter_map(|embed| {
+                if let AppBskyEmbedImagesMain(embed) = embed {
+                    Some(embed.images.iter())
+                } else {
+                    None
+                }
+            })
+            .flatten()
+            .map(|i| {
+                let link = match &i.image {
+                    BlobRef::Typed(TypedBlobRef::Blob(blob_ref)) => &blob_ref.r#ref.link,
+                    BlobRef::Untyped(blob_ref) => &blob_ref.cid,
+                };
+
+                (
+                    format!(
+                        "https://cdn.bsky.app/img/feed_thumbnail/plain/{}/{}@jpeg",
+                        "did:plc:mzwculbn44rdeouyzjp4y6gx", link,
+                    ),
+                    &i.alt,
+                )
+            })
+    }
+
+    pub(super) fn ago(&self) -> String {
+        timeago::Formatter::new().convert_chrono(*self.0.created_at.as_ref(), chrono::Utc::now())
+    }
 }
